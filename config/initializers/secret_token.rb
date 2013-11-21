0dffc3893c072376d05fd0eb1048a8c65fa42549
Sarah Moore <sfmoore99@gmail.com>
2013-11-21 14:21:03 -0500

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SatApp::Application.config.secret_key_base = '55c172867bdb2095d6db0ebeeb95cd449ea2fe4d7844f699b2d37a6e4e6bfccbc1caab23c85a11fae684ba1b70aaf70d3f3b782a7726f6b96f32407ac8d41ec5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SatApp::Application.config.session_store :cookie_store, key: '_sat_app_session'
