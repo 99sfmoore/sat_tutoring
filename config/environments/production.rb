bfc6cb3966f903b78cf98e279c0716fa430f95e3
Sarah Moore <sfmoore99@gmail.com>
2014-01-10 00:49:37 -0500

    add mailgun configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,15 @@ SatApp::Application.configure do
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies
   config.force_ssl = true
+
+  #for Mailgun
+  ActionMailer::Base.smtp_settings = {
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'whispering-scrubland-4889.heroku.com',
+    :authentication => :plain,
+  }
+  ActionMailer::Base.delivery_method = :smtp
 end
