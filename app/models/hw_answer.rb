b4856032c771341cf3aa0cfa695966d9451516cb
Sarah Moore <sfmoore99@gmail.com>
2014-01-09 22:42:23 -0500

    fix migration

--- app/models/hw_answer.rb
+++
@@ -1,6 +0,0 @@
-class HwAnswer < ActiveRecord::Base
-  belongs_to :student
-  belongs_to :hw_question
-
-  
-end
--- app/models/hw_assignment.rb
+++
@@ -1,4 +0,0 @@
-class HwAssignment < ActiveRecord::Base
-  belongs_to :category
-  has_many :hw_questions
-end
