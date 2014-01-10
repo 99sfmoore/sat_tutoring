8bf0ac4d61f8836f415e1a422cfd62900fd78ae9
Sarah Moore <sfmoore99@gmail.com>
2014-01-09 23:38:24 -0500

    attempt to fix heroku css

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,7 @@ module SatApp
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.assets.enable = true #THIS IS TO FIX HEROKU CSS
     config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
   end
 end
