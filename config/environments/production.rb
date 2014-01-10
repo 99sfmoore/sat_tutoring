e7daf21ac2d6bd1c5f6fe1f1963d367ee40c7845
Sarah Moore <sfmoore99@gmail.com>
2014-01-10 01:00:02 -0500

    update app domain name in mailgun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -98,7 +98,7 @@ SatApp::Application.configure do
     :address        => ENV['MAILGUN_SMTP_SERVER'],
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'whispering-scrubland-4889.heroku.com',
+    :domain         => 'nycares-sat.herokuapp.com',
     :authentication => :plain,
   }
   ActionMailer::Base.delivery_method = :smtp
