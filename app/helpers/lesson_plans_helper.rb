9123eb16b5ccfea2856cda8b4050fc02c4fa5bca
Sarah Moore <sfmoore99@gmail.com>
2014-02-14 14:46:23 -0500

    Fix bug in Vocab email

--- app/helpers/lesson_plans_helper.rb
+++ app/helpers/lesson_plans_helper.rb
@@ -4,7 +4,7 @@ module LessonPlansHelper
     current_user.lesson_plans.select {|lp| lp.date < Date.current}.sort_by{|lp| lp.number}.last
   end
 
-  def default_message(segment)
-    "When you finish the #{segment.name} homework, please \"Reply All\" to send me your answers."
+  def default_message(subject)
+    "When you finish the #{subject} homework, please \"Reply All\" to send me your answers.\n\nSee you next week!"
   end
 end
